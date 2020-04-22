c46ea0df2b5deb3a5eb3113e7a7333f52e28636f
Matt Andrews <matt@carbonbasedcreative.com>
2020-04-22 06:53:05 -0600

    Start building components

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Again</Text>
-    </View>
+    <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ececec',
-  },
-});
