5250869fe27131a485dac1085fbea11dbb3bf9c0
Matt Andrews <matt@carbonbasedcreative.com>
2020-04-29 21:46:35 -0600

    Start of wiring up components

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React , { Component } from 'react';
 import { ScrollView, FlatList, Text } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners:state.partners
+    };
+};
 
 const Mission = () => {
     return(
@@ -20,12 +27,6 @@ const Mission = () => {
 }
 
 class About extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -38,7 +39,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{ source: {uri: baseUrl + item.image }}}
                 />
             );
         };
@@ -50,7 +51,7 @@ class About extends Component {
                     title="Community Partners"
                 >
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         keyExtractor={item => item.id.toString()}
                         renderItem={renderPartner}
                     />
@@ -62,4 +63,4 @@ class About extends Component {
 
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
