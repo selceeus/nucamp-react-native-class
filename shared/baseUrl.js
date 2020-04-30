ac7fa1e60080de20bd1547d7eecea308e636222b
Matt Andrews <matt@carbonbasedcreative.com>
2020-04-29 21:19:14 -0600

    Setting up Redux

---
+++ shared/baseUrl.js
@@ -0,0 +1,5 @@
+//json-server --watch db.json -p 3001 -d 2000 -H 10.0.0.3
+//json-server --watch db.json -p 3001 -d 2000
+//json-server --host 10.0.0.3 
+
+export const baseUrl = 'http://10.0.0.3>:3001/';
