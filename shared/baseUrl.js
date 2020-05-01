25f489de16067512e0a7c6115a4149b4654132de
Matt Andrews <matt@carbonbasedcreative.com>
2020-04-30 21:42:46 -0600

    Finished wiring up Redux to all components

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,5 +1,5 @@
-//json-server --watch db.json -p 3001 -d 2000 -H 10.0.0.3
+//json-server --watch db.json -p 3001 -d 2000 -H 10.0.0.44
 //json-server --watch db.json -p 3001 -d 2000
 //json-server --host 10.0.0.3 
 
-export const baseUrl = 'http://10.0.0.3>:3001/';
+export const baseUrl = 'http://10.0.0.44:3001/';
